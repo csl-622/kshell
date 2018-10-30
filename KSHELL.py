47057edcd62c78cdc1aca3dbcb4ddb7f401c9853
RoySoumyadeep <soumya96roy@gmail.com>
2018-10-30 23:46:16 +0530

    Add seperated folders for DATASET and OUTPUT and also did some commenting in the code

--- KSHELL.py
+++ KSHELL.py
@@ -46,28 +46,51 @@ def find_set(H,deg):
 ############################################################################
 
 ## VARIABLES
+G = 0
+H = 0
+temp = []
+buckets = []
+deg = 1
 no_of_Hindex = 4
 actual_max_shell = 0
+dict_shell = {}
+no_run = 30
+percent_of_nodes = 1.0
+percent_nodes = 0 
+current = 0
+max_shell = 0
+neighbour_max_shell = 0
+neighbour_max_shell_value = 0
 
-## CREATING DIGRAPH 
-G = nx.read_adjlist("DATASET1/dataset1.txt",create_using=nx.Graph(), nodetype=int)
 
+## ( creating digraph )
+G = nx.read_adjlist("DATASET/1/dataset1.txt",create_using=nx.Graph(), nodetype=int)
+
+## ( Updating the variables )
 nodes_list = list(G.nodes())
 edges_list = list(G.edges())
 no_nodes = len(nodes_list)
 no_edges = len(edges_list)
+percent_nodes = (int)((percent_of_nodes/100.0)*no_nodes)
 
-## CREATING A DICTIONARY FOR NODES AND POSITION
+
+## ( Creating Dictionary with nodes as keys and its position in list as values )
 dict_nodes = {}
 
 for i in range(0,no_nodes,1):
 	dict_nodes[nodes_list[i]] = i
 
+## ( Creating a nested list storing node and its consecutive h-index data )
+#   Example = [ 24 , 20 , 19 , 18 , 17 , 15 , 13 ]
+#	Here : 	24 = Node number
+#			20 = h0 index i.e degree
+#			19 = h1 index
+#			18 = h2 index ....
+#			13 = shell number
 node_details = [ [nodes_list[i],0,0,0,0,0,0] for i in range(0,no_nodes,1)]
 
-
+###### CALCULATNG H INDEX OF NODES
 for h in range(0,no_of_Hindex+1,1):
-
 	for i in range(0,no_nodes,1):
 		neighbours = [m for m in G[node_details[i][0]]]
 		degree = len(neighbours)
@@ -86,17 +109,12 @@ for h in range(0,no_of_Hindex+1,1):
 			node_details[i][h+1] = H_index(degree, H_value_of_neighbours)
 
 
-##### GETTING THE SHELL NUMBER OF THE NODES
+## ( Creating copy of graph for getting the shell number of nodes )
 H = G.copy()
 
-temp = [] # for the currently filled bucket
-buckets = [] #list of lists(buckets)
-deg = 1 # for degree of nodes
-
+###### CALCULATING SHELL NUMBER OF NODES
 while(True):
-	
 	count = check(H,deg)
-
 	if ( count == 1 ):
 		shell_set = find_set(H,deg)
 		for j in shell_set:
@@ -108,66 +126,56 @@ while(True):
 		buckets.append(temp)
 		temp = [] # start with new temp array
 
-
 	if (len(H.nodes()) == 0):
 		buckets.append(temp)
 		break
 
-#####
-dict_shell = {}
-
+## ( Creating Dictionary with nodes as keys and its shell number as values )
 for i in range(0,len(buckets),1):
 	for j in buckets[i]:
 		node_details[dict_nodes[j]][6] = i+1
 		dict_shell[j] = i+1
 
+## ( Displaying the details of nodes in graph )
 # print("==========================================")
 # for i in node_details:
 # 	print(i)
 # print("==========================================")
 
 
-
-
-## VARIABLES
-no_run = 2
-percent_of_nodes = 1.0
-percent_nodes = (int)((percent_of_nodes/100.0)*no_nodes)
-current = 0
-max_shell = 0
-neighbour_max_shell = 0
-neighbour_max_shell_value = 0
-
+## ( Updating variable and displaying max shell number in graph )
 actual_max_shell = len(buckets) + 1
 print("ACTUAL MAX SHELL : "+str(actual_max_shell))
 
+
 ###### RANDOM WALK
-print("=========================================")
-print("RANDOM WALK :")
-print("=========================================")
-for i in range(0,no_run,1):
-	print("--------------------------------------")
-	print("RUN NO  : "+str(i+1))
-	current = random.choice(nodes_list)
-	max_shell = dict_shell[current] + 1
+# print("=========================================")
+# print("RANDOM WALK :")
+# print("=========================================")
 
-	for j in range(1,percent_nodes,1):
-		print(str(current)+":"+str(dict_shell[current]+1)),
-		neighbours = [m for m in G[current]]
-		current = random.choice(neighbours)
-		if ( max_shell < dict_shell[current] + 1 ):
-			max_shell = dict_shell[current] + 1
-		print("->"),
-	print()
-	print("MAX SHELL REACHED : "+str(max_shell))
+# # ( Random walk main process )
+# for i in range(0,no_run,1):
+# 	print("--------------------------------------")
+# 	print("RUN NO  : "+str(i+1))
+# 	current = random.choice(nodes_list)
+# 	max_shell = dict_shell[current] + 1
 
-
-print()
+# 	for j in range(1,percent_nodes,1):
+# 		print(str(current)+":"+str(dict_shell[current]+1)),
+# 		neighbours = [m for m in G[current]]
+# 		current = random.choice(neighbours)
+# 		if ( max_shell < dict_shell[current] + 1 ):
+# 			max_shell = dict_shell[current] + 1
+# 		print("->"),
+# 	print()
+# 	print("MAX SHELL REACHED : "+str(max_shell))
 
 ###### HILL CLIMBING
 print("=========================================")
 print("HILL CLIMBING :")
 print("=========================================")
+
+## ( Hill climbing main process )
 for i in range(0,no_run,1):
 	print("--------------------------------------")
 	print("RUN NO  : "+str(i+1))
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # kshell
+
+1. Calculated the H-index of nodes from a Graph
+2. Calculated the Shell number of the nodes from a graph
+3. Perfromed Initial random walk on the graph
+4. Performed Initial hill climbing on the graph
