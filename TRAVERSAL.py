3bb14e0a9cac253a620ed056ea9ecbb31dc725a0
RoySoumyadeep <soumya96roy@gmail.com>
2018-11-01 16:57:49 +0530

    Operation for different nodes percent

--- TRAVERSAL.py
+++ TRAVERSAL.py
@@ -1,8 +1,11 @@
 import os
 
 no_of_dataset = 11
+percent_node_list = [1,5,10,15]
 
 for i in range(1,no_of_dataset+1,1):
 	print("CURRENTLY RUNNING DATASET : "+str(i))
-	os.system("python KSHELL.py "+str(i))
+	for j in percent_node_list:
+		print("===============> PERCENT NODE : "+str(j))
+		os.system("python KSHELL.py "+str(i)+" "+str(j))
 print("FINISHED")
