85434ba421c09108baa4e1bf160b96b629861620
RoySoumyadeep <soumya96roy@gmail.com>
2018-11-23 23:21:30 +0530

    Added functionality for part wise code execution

--- TRAVERSAL.py
+++ TRAVERSAL.py
@@ -1,11 +1,16 @@
 import os
 
-no_of_dataset = 11
-percent_node_list = [1,5,10,15]
+no_of_dataset = 1
+percent_node_list = [1]
+part_1 = 0
+part_2 = 0
+part_3 = 1
+
 
 for i in range(1,no_of_dataset+1,1):
 	print("CURRENTLY RUNNING DATASET : "+str(i))
 	for j in percent_node_list:
 		print("===============> PERCENT NODE : "+str(j))
-		os.system("python KSHELL.py "+str(i)+" "+str(j))
+		os.system("python KSHELL.py "+str(i)+" "+str(j)+" "+str(part_1)+" "+str(part_2)+" "+str(part_3))
+
 print("FINISHED")
