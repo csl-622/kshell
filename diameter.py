42ca457db4751445aaa6b2c17f50d0866d832d02
Jagadeesh <36622694+Jagadeesh-Cha@users.noreply.github.com>
2018-11-24 15:01:38 +0530

    Update diameter.py

--- diameter.py
+++ diameter.py
@@ -3,69 +3,69 @@ import matplotlib.pyplot as ply
 import random
 import numpy as np
 
-def check_existence(H,d):
-        f = 0
-        for each in H.nodes():
-                  if H.degree(each) <= d:
-                           f =1
-                           break
-        return f				      
-        
-def find(H,it):
-       set1=[]
-       for each in H.nodes():
-                if H.degree(each) <= it :
-                            set1.append(each)	
-       return set1						
-
 G=nx.Graph() 
-G = nx.read_edgelist('graph_name.txt', nodetype = int)
+G = nx.read_edgelist('Gowalla_edges.txt', nodetype = int)
 n = len(G.nodes())
-
 print("no of nodes in graph:"+str(n))
 t = int(n/100)
 N=list(G.nodes())
+H1=[]
+H2=[]
+D=[]
+DDD=[]
+DD=[]
+index=0
+''' Finding the H1 indeces of the nodes of graph'''
+for i in range(n):
+      P = list(G.neighbors(N[i]))
+      for j in range(len(P)):
+            D.append(G.degree(P[j]))
+      D.sort()
+      myset=set(D)
+      DDD=list(myset)
+      
+      for b in range(len(DDD)):
+      
+            DD.append(D.count(DDD[b])) 
+      for h in range(len(DDD)):
+            if( DD[h] >= DDD[h] ):index=DDD[h]	
+      H1.append(index)
+      D=[]
+      DD=[]
+      DDD=[]
+      index = 0
+    
+''' Finding the H2 indeces of the nodes of graph '''
 
-'''  Finding the buckets '''
-H = G.copy()
-it = 1
-tmp = []
-buckets = []
-shell_no=[]
-while(1):
-          flag =check_existence(H, it)
-          if flag == 0:
-                    it = it+1
-                    buckets.append(tmp)
-                    tmp = []
-          if flag ==1:
-                    node_set = find(H,it)
-                    for each in node_set:
-                             H.remove_node(each)
-                             tmp.append(each)
-          if H.number_of_nodes() == 0:
-                    buckets.append(tmp)
-                    break
-
-k=len(buckets)
-print("no of shells: "+str(k))
-max_shell_no = k
-for i in N:
-         shell_no.append(i)
-                  
-for j in range(n):
-      for r in range(k):
-            p=len(buckets[r])
-            for y in range(p):
-                  if (N[j] == buckets[r][y]) : shell_no[j] = r+1    			
-v = (list)(G.nodes()) 
+E=[]
+EE=[]
+EEE=[]
+for i in range(n):
+      R = list(G.neighbors(N[i]))
+      for j in range(len(R)):
+            E.append(H1[N.index(R[j])])
+      E.sort()
+      mset=set(E)
+      EEE=list(mset)
+      
+      for k in range(len(EEE)):
+      
+            EE.append(E.count(EEE[k])) 
+      for h in range(len(EEE)):
+            if( EE[h] >= EEE[h] ):index=EEE[h]	
+      H2.append(index)
+      E=[]
+      EE=[]
+      EEE=[]
+      index = 0
+maximum = max(H2)
 smallest_paths = []
 i=0
 j=0
-for i in range(len(v)-1):
-       for j in range(i+1, len(v)):
+for i in range(len(N)-1):
+       for j in range(i+1, len(N)):
              
-             if(nx.has_path(G,v[i],v[j]) == True):smallest_paths.append(nx.shortest_path(G,v[i],v[j]))
+             if(nx.has_path(G,N[i],N[j]) == True):smallest_paths.append(nx.shortest_path(G,N[i],N[j]))
 smallest_paths.sort(key=len)			 
 
 d = len(smallest_paths[len(smallest_paths)-1]) - 1
@@ -82,8 +82,8 @@ for i in range(len(diameter)):
       diameter_shells = []
       number = 0
       for j in range(len(diameter[i])):
-            diameter_shells.append(shell_no[diameter[i][j]])
-            if(shell_no[diameter[i][j]] == k) : number = number +1
+            diameter_shells.append(H2[diameter[i][j]])
+            if(H2[diameter[i][j]] == maximum) : number = number +1
       inner_encountered.append(number)	
       print(i)	
       print(diameter_shells)
@@ -91,5 +91,7 @@ for i in range(len(diameter)):
 avg = sum(inner_encountered)/len(inner_encountered)
 print(inner_encountered)
 print('no of times on an average the diameter encountered the inner shell is : '+str(avg))
+
+
 
 
