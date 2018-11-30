7c73f44afd2806df89315a4fe8bed1d16236e7fc
Jagadeesh <36622694+Jagadeesh-Cha@users.noreply.github.com>
2018-11-30 15:46:57 +0530

    final code of hillclimbing as in paper
    implemented the SIR method in this code

--- hill_climbing_SIR.py
+++ hill_climbing_SIR.py
@@ -9,6 +9,7 @@ n = len(G.nodes())
 print("no of nodes in graph:"+str(n))
 t = int(n/100)
 N=list(G.nodes())
+W = list(G.nodes())
 H1=[]
 H2=[]
 D=[]
@@ -57,15 +58,15 @@ for i in range(n):
       EEE=[]
       index = 0
 q=0
-print("no of shells in the graph are : "+str(len(list(set(H2)))))
+print("no of different H2 indeces in the graph are : "+str(len(list(set(H2)))))
 #walk
-visited = []
 max_encountered_list = []
 walk=[]
 shell_list=[]
 shell_set_list=[]
 distint_shell_list=[]
 F=[]
+f=0
 no_of_steps=[]
 H2_1=[]
 #k = len(buckets)
@@ -77,41 +78,51 @@ maximum = max(H2)
 total = 0  
 steps = []
 #print(H2_1)	  
+visited = []
+unvisited = list(G.nodes())
+M_unvisited =[] 
+e=0
 while(q<50): 
           start = random.choice(H2_1)
           r=0
           repeat_count = 0
           
-          while ((H2[N.index(start)] < maximum ) and (r<t)):
+          while ((H2[W.index(start)] < maximum ) and (r<t)):
                
                walk.append(start)
-               visited.append(start)
+               if(start in unvisited):visited.append(start)
+               if(start in unvisited):unvisited.remove(start)
                M=list(G.neighbors(start))
-               if(len(M)==0):
+               for e in range(len(M)):
+                      if (M[e] in unvisited):M_unvisited.append(M[e]) 
+               if(len(M_unvisited)==0):
                       start = random.choice(H2_1)
-                      visited.append(start)
+                      if(start in unvisited):unvisited.remove(start)
                       repeat_count = repeat_count+1
-               for j in range(len(M)):
-                      F.append(H2[N.index(M[j])])
+               for j in range(len(M_unvisited)):
+                      F.append(H2[W.index(M_unvisited[j])])
                #print(F)
                ind = max([i for i,j in enumerate(F) if j == max(F)])		  
                F=[]
-               if((H2[N.index(M[ind])] > H2[N.index(start)]) and ((N.index(M[ind])) not in visited)):                   
-                      start = M[ind]
-                      visited.append(start)
+               if((H2[W.index(M_unvisited[ind])] > H2[W.index(start)])):                   
+                      start = M_unvisited[ind]
+                      if(start in unvisited):unvisited.remove(start)
                else:
                       repeat_count  =  repeat_count+1
-                      start = M[ind]
-                      visited.append(start)
+                      start = random.choice(M_unvisited)
+                      if(start in unvisited):unvisited.remove(start)
                r = r+1
-          if(repeat_count == t):print(str(q)+" walk :failed to reach the inner shell")
+          if(repeat_count == t):
+               print(str(q)+" walk :failed to reach the inner shell")
+               f=f+1
           else:
                 total = total + r
                 
                 steps.append(r)
                 print(str(q)+" walk: the number of steps taken to reach the inner shell are = "+str(r))
-                print("no of times it got stuck at the local maximum or for not having neighbors or for visiting visited nodes is: "+str(repeat_count))
+                print("no of times it got stuck at the local maximum or for not having neighbors is: "+str(repeat_count))
           q=q+1
 print("avg no of steps taken in an iteration to reach inner most shell are: "+str(total/q))
 mode = max(set(steps), key=steps.count)       
 print("mode of the list of  no of steps taken to reach inner shell is: "+str(mode))  
+print("no of failures to reach the inner shell in 50 attemps are: "+str(f))
